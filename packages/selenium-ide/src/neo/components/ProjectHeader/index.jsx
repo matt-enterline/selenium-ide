f2c2d5b3f675a7e3044a5ae6d7c3c0fb10a3013a
Matt Enterline <menterline@cargas.com>
2020-05-18 08:37:28 -0400

    Renamed manifest and title bar

--- packages/selenium-ide/src/neo/components/ProjectHeader/index.jsx
+++ packages/selenium-ide/src/neo/components/ProjectHeader/index.jsx
@@ -57,7 +57,7 @@ export default class ProjectHeader extends React.Component {
     return (
       <div className={classNames('header', { changed: this.props.changed })}>
         <Title
-          title={`Selenium IDE ${this.props.title === '' ? '' : '-'} ${
+          title={`Cargas Selenium IDE ${this.props.title === '' ? '' : '-'} ${
             this.props.title
           }${this.props.changed ? '*' : ''}`}
         />
